aaa37070d2840521aa6009b687001469247490b9
Alexey <h33el@yandex.ru>
2020-09-23 11:22:47 +0300

    sem2 enum improvement

--- sem2/src/enum.cpp
+++ sem2/src/enum.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
 
-enum Operations {
+enum class Operations : int {
     // По умолчанию начинается с нуля, но можно задать начальное значение
-    add = 1,
+    Add = 1,
     // Далее элементы инкриментируются
-    subtract, //2
+    Subtract, //2
     // Можно переопределить
-    multiply = 4
+    Multiply = 4
 };
 
 int main() {
     using namespace std;
 
-    int operation;
     int x1;
     int x2;
     int result;
+    int operationInt;
 
-    cout << "Add: 1 \tSubtract: 2 \tMultiply: 4" << endl;
+    cout << "Add: " << static_cast<int>(Operations::Add) << "\t";
+    cout << "Subtract: " << static_cast<int>(Operations::Subtract) << "\t";
+    cout << "Multiply: " << static_cast<int>(Operations::Multiply) << endl;
+
     cout << "Input x1: " << endl;
     cin >> x1;
     cout << "Input x2: " << endl;
     cin >> x2;
     cout << "Input operation number: " << endl;
-    cin >> operation;
+    cin >> operationInt;
 
-    switch (operation) {
-        case Operations::add:
+    auto operationEnum = static_cast<Operations>(operationInt);
+
+    switch (operationEnum) {
+        case Operations::Add:
             result = x1 + x2;
             break;
-        case Operations::subtract:
+        case Operations::Subtract:
             result = x1 - x2;
             break;
-        case Operations::multiply:
+        case Operations::Multiply:
             result = x1 * x2;
             break;
         default:
